d121056bcd6fe4369cfd45df35907f7b5748bfee
VignanBaligari234 <114463516+VignanBaligari234@users.noreply.github.com>
2024-09-17 22:42:20 +0530

    vpc module advanced

---
+++ variables.tf
@@ -0,0 +1,73 @@
+variable "cidr_block" {
+  
+}
+
+variable "enable_dns_hostnames" {
+  default = true
+}
+
+variable "enable_dns_support" {
+  default = true
+}
+
+variable "common_tags" {
+  type = map 
+  default = {}
+}
+
+variable "vpc_tags" {
+  type = map 
+  default = {}
+}
+
+variable "project_name" {
+
+}
+
+variable "igw_tags" {
+  default = {}
+}
+
+variable "public_subnet_cidr" {
+  type = list  
+  validation {
+    condition = length(var.public_subnet_cidr) == 2
+    error_message = "please provide 2 public subnet CIDR"
+  }
+}
+
+variable "private_subnet_cidr" {
+  type = list  
+  validation {
+    condition = length(var.private_subnet_cidr) == 2
+    error_message = "please provide 2 private subnet CIDR"
+  }
+}
+
+variable "database_subnet_cidr" {
+  type = list  
+  validation {
+    condition = length(var.database_subnet_cidr) == 2
+    error_message = "please provide 2 database subnet CIDR"
+  }
+}
+
+variable "nat_gateway_tags" {
+  default = {}
+}
+
+variable "public_route_table_tags" {
+  default = {}
+}
+
+variable "private_route_table_tags" {
+  default = {}
+}
+
+variable "database_route_table_tags" {
+  default = {}
+}
+
+variable "db_subnet_group_tags" {
+  default = {}
+}
